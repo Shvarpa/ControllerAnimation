349bae6fbc076e7f911f826c7c0f0803915f1b06
Pavel Shvarchov <Shvarpa@gmail.com>
2020-02-06 16:28:27 +0200

    fixing package

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import Controller from "./svelte/Controller.svelte"
-import XboxController from './svelte/XboxController.svelte'
-import DS4Controller from './svelte/DS4Controller.svelte'
-
-export default { Controller, XboxController, DS4Controller };
+export { default as Controller } from "./svelte/Controller.svelte";
+export { default as XboxController } from './svelte/XboxController.svelte';
+export { default as DS4Controller } from './svelte/DS4Controller.svelte';
